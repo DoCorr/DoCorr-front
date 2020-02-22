ba38e8c55c0031c2a64905f7916dbb717d6f5858
hj_macbook <rjcnd123@gmail.com>
2020-02-22 17:39:08 +0900

    eslint, prettier 세팅 및 Folder Tree 세팅

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  presets: [
+    [
+      '@emotion/babel-preset-css-prop',
+      {
+        autoLabel: true,
+        labelFormat: '[local]'
+      }
+    ]
+  ]
+}
